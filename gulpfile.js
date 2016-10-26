0bd1805909800fa002cea3a73b73930cd285a450
KevinAdu <kevyugi@gmail.com>
2016-10-26 23:27:33 +0900

    Added task to clean the dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
+var del = require('del');
+var sequence = require('run-sequence');
 
 var paths = {
   fonts: ['src/fonts/**/*']
 };
 
-gulp.task('fonts', function() {
-  return gulp.src(paths.fonts)
-    .pipe(gulp.dest('dist/fonts'))
+gulp.task('clean', function() {
+  return del('dist');
 });
 
-gulp.task('default', ['fonts']);
+gulp.task('fonts', function() {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('default', function() {
+  sequence('clean', 'fonts');
+});
